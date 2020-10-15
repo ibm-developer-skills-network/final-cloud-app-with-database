54f6ad8880ec9e2db9998ca1b16a1061d62fd470
yluo@ca.ibm.com <yluo@ca.ibm.com>
2020-10-15 15:53:42 -0400

    extended model

--- data/models.py
+++ data/models.py
@@ -6,6 +6,7 @@ except Exception:
     sys.exit()
 
 
+
 # User model
 class User(models.Model):
     first_name = models.CharField(null=False, max_length=30, default='john')
@@ -18,10 +19,10 @@ class User(models.Model):
 
 # Learner model
 class Learner(User):
-    STUDENT = 'STU'
-    DEVELOPER = 'DEV'
-    DATA_SCIENTIST = 'DS'
-    DATABASE_ADMIN = 'DBA'
+    STUDENT = 'student'
+    DEVELOPER = 'developer'
+    DATA_SCIENTIST = 'data_scientist'
+    DATABASE_ADMIN = 'dba'
     OCCUPATION_CHOICES = [
         (STUDENT, 'Student'),
         (DEVELOPER, 'Developer'),
@@ -30,13 +31,55 @@ class Learner(User):
     ]
     occupation = models.CharField(
         null=False,
-        max_length=3,
+        max_length=20,
         choices=OCCUPATION_CHOICES,
-        default=STUDENT,
+        default=STUDENT
     )
     social_link = models.URLField(max_length=200)
 
     def __str__(self):
-        return self.first_name + "," + self.last_name + ", " + self.occupation + ", " + self.social_link
+        return self.first_name + ", " + self.last_name + ", " + self.occupation + ", " + self.social_link
 
 
+# Instructor model
+class Instructor(User):
+    full_time = models.BooleanField(default=True)
+    total_learners = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name + ", " + self.last_name + ", " + str(self.full_time) + ", " + str(self.total_learners)
+
+
+# Course model
+class Course(models.Model):
+    name = models.CharField(null=False, max_length=30, default='online course')
+    description = models.CharField(max_length=500)
+    instructors = models.ManyToManyField(Instructor)
+    learners = models.ManyToManyField(Learner, through='Enrollment')
+
+    def __str__(self):
+        return self.name + "," + self.description
+
+
+# Enrollment model
+class Enrollment(models.Model):
+    AUDIT = 'audit'
+    HONOR = 'honor'
+    COURSE_MODES = [
+        (AUDIT, 'Audit'),
+        (HONOR, 'Honor'),
+    ]
+    learner = models.ForeignKey(Learner, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date_enrolled = models.DateField()
+    mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
+
+
+# Project
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    grade = models.FloatField(default=0.3)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ", " + str(self.grade) + ", " + self.course.name
--- main.py
+++ main.py
@@ -11,24 +11,85 @@ from data.models import *
 from datetime import date
 
 
-# Delete all
-User.objects.all().delete()
-Learner.objects.all().delete()
-
-#Add user
-user = User(first_name='Yan', last_name='Luo', dob=date(1962, 7, 16))
-user.save()
-
-
-
-# Application logic
-first_user = User.objects.all()[0]
-print(first_user)
-
-learner = Learner(first_name='learner', last_name='learner', dob=date(1962, 7, 16), occupation='DBA',
-                  social_link='https://www.linkedin.com/feed/')
-learner.save()
+def clean_data():
+    # Delete all
+    User.objects.all().delete()
+    Learner.objects.all().delete()
+    Instructor.objects.all().delete()
+    Course.objects.all().delete()
+    Enrollment.objects.all().delete()
 
 
-first_learner = Learner.objects.all()[0]
-print(first_learner)
+def populate_database():
+    # Add user
+    user = User(first_name='John', last_name='Doe', dob=date(1962, 7, 16))
+    user.save()
+    # Add Learners
+    learner_james = Learner(first_name='James', last_name='Smith', dob=date(1962, 7, 16),
+                            occupation='data_scientist',
+                            social_link='https://www.linkedin.com/james/')
+    learner_james.save()
+    learner_mary = Learner(first_name='Mary', last_name='Smith', dob=date(1991, 6, 12), occupation='dba',
+                           social_link='https://www.facebook.com/mary/')
+    learner_mary.save()
+    learner_robert = Learner(first_name='Robert', last_name='Lee', dob=date(1999, 1, 2), occupation='developer',
+                             social_link='https://www.facebook.com/robert/')
+    learner_robert.save()
+
+    learners = Learner.objects.all()
+    print(learners)
+    # Add Instructors
+    instructor_yan = Instructor(first_name='Yan', last_name='Luo', dob=date(1962, 7, 16),
+                                full_time=True,
+                                total_learners=100)
+    instructor_yan.save()
+    instructor_joy = Instructor(first_name='Joy', last_name='Li', dob=date(1992, 1, 2),
+                                full_time=False,
+                                total_learners=100)
+    instructor_joy.save()
+    instructor_peter = Instructor(first_name='Peter', last_name='Chen', dob=date(1982, 5, 2),
+                                  full_time=True,
+                                  total_learners=100)
+    instructor_peter.save()
+
+    instructors = Instructor.objects.all()
+    print(instructors)
+
+    # Add Courses
+    course_cloud_app = Course(name="Cloud Application Development with Database",
+                              description="Develop and deploy application on cloud")
+    course_cloud_app.save()
+    course_python = Course(name="Introduction to Python",
+                           description="Learn core concepts of Python and obtain hands-on experience via a capstone project")
+    course_python.save()
+    course_cloud_app.instructors.add(instructor_yan)
+    course_cloud_app.instructors.add(instructor_joy)
+
+    print(course_cloud_app.instructors.all())
+
+    course_python.instructors.add(instructor_peter)
+    courses = Course.objects.all()
+    print(courses)
+
+    james_cloud = Enrollment.objects.create(learner=learner_james, date_enrolled=date(2020, 8, 1),
+                                            course=course_cloud_app, mode='audit')
+    james_cloud.save()
+    mary_cloud = Enrollment.objects.create(learner=learner_mary, date_enrolled=date(2020, 8, 2),
+                                           course=course_cloud_app, mode='honor')
+    mary_cloud.save()
+
+    robert_python = Enrollment.objects.create(learner=learner_robert, date_enrolled=date(2020, 9, 2),
+                                              course=course_python, mode='honor')
+    robert_python.save()
+    print(course_python.learners.all())
+
+    project_orm = Project(name="Object-relational mapping project", grade=0.2, course=course_cloud_app)
+    project_django = Project(name="Django full stack project", grade=0.2, course=course_cloud_app)
+    project_python = Project(name="Python final project", grade=0.5, course=course_python)
+    print(project_orm)
+    print(project_orm.course)
+    print(project_python.course)
+
+
+clean_data()
+populate_database()
