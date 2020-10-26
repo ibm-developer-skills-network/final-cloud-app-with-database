15b559cd2eb8589def7046dc074b9869ae53bc05
yluo@ca.ibm.com <yluo@ca.ibm.com>
2020-10-26 18:42:16 -0400

    minor update

--- main.py
+++ main.py
@@ -96,8 +96,11 @@ def populate_database():
     robert_python.save()
 
     project_orm = Project(name="Object-relational mapping project", grade=0.2, course=course_cloud_app)
+    project_orm.save()
     project_django = Project(name="Django full stack project", grade=0.2, course=course_cloud_app)
+    project_django.save()
     project_python = Project(name="Python final project", grade=0.5, course=course_python)
+    project_python.save()
 
     print("Course: {}".format(course_cloud_app))
     print("Course instructors: {}".format(course_cloud_app.instructors.all()))
@@ -106,6 +109,11 @@ def populate_database():
 
 
 def simple_queries():
+
+    #Find all courses
+    courses = Course.objects.all()
+    print(courses)
+
     # Find a single instructor
     instructor_yan = Instructor.objects.get(first_name="Yan")
     print(instructor_yan)
@@ -118,13 +126,20 @@ def simple_queries():
     learners_smith = Learner.objects.filter(last_name="Smith")
     print(learners_smith)
 
-    # Find all full time instructors
-    full_time_instructors = Instructor.objects.exclude(full_time=False)
-    print(full_time_instructors)
+    # Find all part time instructors
+    part_time_instructors = Instructor.objects.filter(full_time=False)
+    print("Part time instructors: ")
+    print(part_time_instructors)
 
     # Find all full time instructors with First Name starts with `Y` and learners count greater than 30000
     full_time_instructors = Instructor.objects.exclude(full_time=False).filter(total_learners__gt=30000).\
         filter(first_name__startswith='Y')
+    print("Full time instructors")
+    print(full_time_instructors)
+
+    full_time_instructors = Instructor.objects.filter(full_time=True, total_learners__gt=30000,
+                                                      first_name__startswith='Y')
+    print("Full time instructors")
     print(full_time_instructors)
 
     # Slicing, OFFSET=1, LIMIT=2
@@ -134,34 +149,6 @@ def simple_queries():
     # Order by
     learners = Learner.objects.order_by('dob')
     print(learners)
-
-
-def span_relationship_queries():
-    # Many-to-One relationship
-    # Find the instructors of Cloud app dev course
-    instructors = Instructor.objects.filter(course__name__contains='Cloud')
-    print(instructors)
-    # Find courses instructed by 'Yan'
-    courses = Course.objects.filter(instructors__first_name='Yan')
-    print(courses)
-    # Check the occupations of the courses taught by instructor Yan
-    courses = Course.objects.filter(instructors__first_name='Yan')
-    occupation_list = set()
-    for course in courses:
-        for learner in course.learners.all():
-            occupation_list.add(learner.occupation)
-    print(occupation_list)
-    # Many-to-Many relationship
-    # Learners and Course Enrollment
-    # Check which courses developers are enrolled in year Aug, 2020 with Honer
-    enrollments = Enrollment.objects.filter(date_enrolled__month=8,
-                                            date_enrolled__year=2020,
-                                            learner__occupation='developer')
-    courses_for_developers = set()
-    for enrollment in enrollments:
-        course = enrollment.course
-        courses_for_developers.add(course.name)
-    print(courses_for_developers)
 
 
 def update_data():
@@ -193,8 +180,61 @@ def update_data():
     print(course_python.learners.all())
 
 
-clean_data()
-populate_database()
-simple_queries()
+def span_relationship_queries():
+    # Many-to-One relationship
+    # Find the instructors of Cloud app dev course
+    instructors = Instructor.objects.filter(course__name__contains='Cloud')
+    print(instructors)
+    # Find courses instructed by 'Yan'
+    courses = Course.objects.filter(instructors__first_name='Yan')
+    print(courses)
+    # Check the occupations of the courses taught by instructor Yan
+    courses = Course.objects.filter(instructors__first_name='Yan')
+    occupation_list = set()
+    for course in courses:
+        for learner in course.learners.all():
+            occupation_list.add(learner.occupation)
+    print(occupation_list)
+    # Many-to-Many relationship
+    course = Course.objects.get(name='Introduction to Python')
+    learners = course.learners.all()
+    print(learners.query)
+    # Learners and Course Enrollment
+    # Check which courses developers are enrolled in year Aug, 2020 with Honer
+    enrollments = Enrollment.objects.filter(date_enrolled__month=8,
+                                            date_enrolled__year=2020,
+                                            learner__occupation='developer')
+    courses_for_developers = set()
+    for enrollment in enrollments:
+        course = enrollment.course
+        courses_for_developers.add(course.name)
+    print(courses_for_developers)
+
+    # Related objects
+    print("One-To-One")
+    learner_david = Learner.objects.get(first_name="David")
+    print(learner_david.user_ptr)
+    user_david = User.objects.get(first_name="David")
+    print(user_david.learner)
+
+    print("One-To-Many")
+    project_orm = Project.objects.get(name__contains="mapping")
+    print(project_orm.course)
+    course = Course.objects.get(name__contains='Cloud')
+    print(course)
+    print(course.project_set.all())
+
+    print("Many-To-Many")
+    instructor_yan = Instructor.objects.get(first_name='Yan')
+    print(instructor_yan.course_set.all())
+    course = Course.objects.get(name__contains='Cloud')
+    print(course.instructors.all())
+
+
+
+
+#clean_data()
+#populate_database()
+#simple_queries()
 span_relationship_queries()
-update_data()
+#update_data()
