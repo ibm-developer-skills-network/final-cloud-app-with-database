ac8b6501fbe908e19ffa22f57895605d63e59877
zambujo <zambuj@gmail.com>
2022-05-04 09:41:12 +0200

    Choice model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -120,13 +120,16 @@ class Question(models.Model):
            return False
 
 
-#  <HINT> Create a Choice Model with:
+#  Lab2: Create a Choice Model with:
     # Used to persist choice content for a question
     # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
-# class Choice(models.Model):
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=300, default="")
+    is_correct = models.BooleanField(default=False)
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
