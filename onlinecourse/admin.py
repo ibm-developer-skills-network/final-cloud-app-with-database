57ab9f6dac37d529fc07ce33265e2d95e541ba08
zambujo <zambuj@gmail.com>
2022-05-04 11:17:24 +0200

    importing new models to admin

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
