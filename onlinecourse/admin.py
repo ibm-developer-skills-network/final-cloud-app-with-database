57bcab2aafab1bbf62d756da5191a2536e057fe1
Freedisch <freeproduc@gmail.com>
2022-08-25 19:21:51 +0200

    register modells

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -28,7 +28,8 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ ChoiceInline ]
 
 # <HINT> Register Question and Choice models here
 
@@ -36,5 +37,4 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Question, QuestionInline)
-admin.site.register(Choice, ChoiceInline)
+admin.site.register(Question, QuestionAdmin)
