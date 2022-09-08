9260f78f0e907d24b93f23ac68ae31d0ef2587fa
ZiyiChen <zy035c@gmail.com>
2022-09-08 18:32:10 +0000

    course_detail_bootstrap, models.py, and registration in admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -21,6 +21,12 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
 
 # <HINT> Register Question and Choice models here
 
@@ -28,3 +34,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
