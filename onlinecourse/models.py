6ea70e4ec7d22c80616aea1c35da48cc94d4b414
ZiyiChen <zy035c@gmail.com>
2022-09-09 06:49:03 +0000

    final version, modification on two templates, and map Question to Course, not Lesson

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -105,7 +105,7 @@ class Question(models.Model):
     # Foreign key to lesson
     # question text
     # question grade/mark
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    Course = models.ForeignKey(Course, on_delete=models.CASCADE, default="")
     question_text = models.CharField(max_length=2000)
     grade = models.IntegerField(default = 0)
 
